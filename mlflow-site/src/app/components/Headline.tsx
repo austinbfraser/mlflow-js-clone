a8e7c6206fe2cef616f753697f6d2b832ec5d88b
Austin Fraser <austinbfraser@gmail.com>
2025-02-23 15:45:28 -0800

    changed Headline

--- mlflow-site/src/app/components/Headline.tsx
+++ mlflow-site/src/app/components/Headline.tsx
@@ -3,7 +3,7 @@ import Button from './Button';
 const Headline = () => {
   return (
     <div className="home" id="headline">
-      <div className="bigHeadline">ML workflow for Javascript</div>
+      <div className="bigHeadline">ML workflow for JavascriptIMG</div>
       <div className="headLineText">
         Harness MLflow&apos;s functionality for your Javascript application with
         MLflow.js
